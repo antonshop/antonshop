5da24962996cb48383c13f5d67d29ff689295e32
taoanton <taoanton@gmail.com>
2011-11-22 10:06:58 +0800

    del img

--- includes/templates/template_locker/template_info.php
+++ includes/templates/template_locker/template_info.php
@@ -8,9 +8,9 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
  */
-$template_name = 'my_template_huise';
+$template_name = 'template_locker';
 $template_version = 'Version 1.0';
-$template_author = 'sun Team';
+$template_author = 'ryan';
 $template_description = '';
-$template_screenshot = 'scr_my_template_huise.jpg';
+$template_screenshot = 'scr_template_locker.jpg';
 ?>
