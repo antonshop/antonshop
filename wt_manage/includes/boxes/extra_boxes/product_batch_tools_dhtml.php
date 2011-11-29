71b7ad160ec38ac654da88da0b07c04dc76ccd60
taoanton <taoanton@gmail.com>
2011-11-29 16:19:58 +0800

    add batch csv

---
+++ wt_manage/includes/boxes/extra_boxes/product_batch_tools_dhtml.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * image_handler_tools_dhtml.php
+ * call to include IH2 link in admin->tools
+ *
+ * @author  Tim Kroeger <tim@breakmyzencart.com>
+ * @copyright Copyright 2005-2006 breakmyzencart.com
+ * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License V2.0
+ * @version $Id: image_handler_tools_dhtml.php,v 1.1 2006/04/11 22:00:55 tim Exp $
+ */
+
+  $za_contents[] = array('text' => '商品批量导入导出', 'link' => zen_href_link('product_batch', '', 'NONSSL'));
